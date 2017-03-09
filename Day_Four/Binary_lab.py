f6a2cc144352a9742797fe805b2c6638edc36e10
mmuchai <muchaimercyw@gmail.com>
2017-03-10 01:01:28 +0300

    Progress on binary search

--- Day_Four/Binary_lab.py
+++ Day_Four/Binary_lab.py
@@ -1,5 +1,20 @@
 class BinarySearch (List):
 	def __init__ (self, a ,b):
+		''' The length of a list and the step, give the endpoint of a range.
+		'''
 		super (BinarySearch, self).__init__(length)
-		self.length = length
-array_ = [num for num in range (b, (a*b)+1 ,b)]
+		array_ = [num for num in range (0, (a*b)+1 ,b)]
+		self.length = len(array_) = self.a
+		self.extend(array_)
+
+    def search(self, value):
+        count = 0
+        first = 0
+        last = len(self) - 1
+            
+        if value == self[first]:
+        	return {'count': count, 'index': first}
+        elif value == self[last]:
+        	return {'count': count, 'index': last}
+
+        	midpoint = (first + last) // 2
