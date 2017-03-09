4aa05f2614f3cc0ade079e69ef0306fa6cb24d1a
mmuchai <muchaimercyw@gmail.com>
2017-03-09 16:10:14 +0300

    API:Fetching specific data of specific id

--- Day_Four/Binary_lab.py
+++ Day_Four/Binary_lab.py
@@ -2,4 +2,4 @@ class BinarySearch (List):
 	def __init__ (self, a ,b):
 		super (BinarySearch, self).__init__(length)
 		self.length = length
-		
+		len(length) = a
--- Day_Three/url_consuming_api.py
+++ Day_Three/url_consuming_api.py
@@ -1,8 +1,8 @@
 import requests
 
 '''Simple command lines to consume public API file'''
- '''Get a response that has user_id: 10'''
-info = requests.get('https://jsonplaceholder.typicode.com/posts?userId = 10').json()
+'''Get a response that has user_id: 10'''
+info = requests.get('https://jsonplaceholder.typicode.com/posts?id=41').json()
 
 user_id = info
 print (user_id)
